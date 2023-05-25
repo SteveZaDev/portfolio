a04a039d9f9c8aa33d66c60b1cec8631fd25baff
SteveZaDev <118739097+SteveZaDev@users.noreply.github.com>
2023-05-25 11:52:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//
+console.log("entered")
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show')
+      console.log(entry)
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el))
