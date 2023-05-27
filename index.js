192fcb4b81b6de8b998dffe33fac46067b6d9cca
SteveZaDev <118739097+SteveZaDev@users.noreply.github.com>
2023-05-27 11:35:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -23,3 +23,46 @@ hiddenElementsLeft.forEach((el) => observer.observe(el))
 
 hiddenElementsRight.forEach((el) => observer.observe(el))
 */
+
+  mailtoEl = document.querySelector(".mailto-link")
+	// Add class to mailto link
+	// Needed to separate the disabling of the default action AND copy email to clipboard
+	$('a[href^=mailto]').addClass('mailto-link');
+
+	let mailto = $('.mailto-link');
+	let messageCopy = 'Click to copy email address';
+	let messageSuccess = 'Email address copied to clipboard';
+	
+//	mailto.append('<span class="mailto-message"></span>');
+//  $('.mailto-message').append(messageCopy);
+  
+  let mailtoMsgEl = document.querySelector(".mailto-msg")
+	
+	// Disable opening your email client. yuk.
+	$('a[href^=mailto]').click(function() {
+		return false; 
+	})
+	
+	// On click, get href and remove 'mailto:' from value
+	// Store email address in a variable.
+	mailto.click(function() {
+		let href = $(this).attr('href');
+		let email = href.replace('mailto:', '');
+		copyToClipboard(email);
+		mailtoMsgEl.innerText=(messageSuccess);
+		setTimeout(function() {
+		mailtoMsgEl.innerText=(messageCopy);	;}, 2000); 
+	});
+	
+
+
+// Grabbed this from Stack Overflow.
+// Copies the email variable to clipboard
+function copyToClipboard(text) {
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
